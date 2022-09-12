8929932c104c60b03348fd11f0cf81d7eb2ee4f4
Matveev Timofey <justshowel@gmail.com>
2022-09-12 04:12:45 +0300

    feat: Implement user persistence (not thoroughly tested).

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,14 +1,28 @@
-import { defineStore } from 'pinia';
+import {defineStore} from 'pinia';
 import UserRequestController from "@/controllers/userRequestController";
+
+const defaultUserData = {token: null};
+
+function getLocalUser(prefix) {
+    const user = localStorage.getItem(`${prefix}--user`);
+    if (user)
+        return JSON.parse(user);
+    // To be filled with other data later
+    return defaultUserData;
+}
+
+function setLocalUser(prefix, data) {
+    console.log('Save user');
+    localStorage.setItem(`${prefix}--user`, JSON.stringify(data));
+}
 
 export const useAuthStore = defineStore('auth', {
     state: () => ({
         userRequestController: new UserRequestController('http://markwebdev.ru/api/v1'),
-        token: null,
-        user: null
+        user: defaultUserData
     }),
     getters: {
-        isAuthorised: (state) => state.token !== null
+        isAuthorised: (state) => state.user?.token !== undefined && state.user.token !== null
     },
     actions: {
         /**
@@ -21,7 +35,7 @@ export const useAuthStore = defineStore('auth', {
         async authenticate(email, password) {
             try {
                 const token = await this.userRequestController.login(email, password);
-                this.token = token;
+                this.user.token = token;
                 this.userRequestController.setToken(token);
                 return {success: true, errors: ["Logged in!"]}
             }
@@ -46,6 +60,25 @@ export const useAuthStore = defineStore('auth', {
             catch (e) {
                 return {success: false, errors: e};
             }
+        },
+
+        /**
+         * Attempt to recover user data from localhost
+         */
+        tryRememberUser() {
+            console.log('Remember user');
+            this.user = getLocalUser('vue-cloud-storage');
+            if (this.user.token)
+                this.userRequestController.setToken(this.user.token);
         }
     },
-})
+});
+
+setTimeout(() => {
+    const store = useAuthStore();
+    store.tryRememberUser();
+    store.$subscribe((
+        mutation,
+        state) => setLocalUser('vue-cloud-storage', state.user));
+}, 0);
+
